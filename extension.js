c0ce69ad250d1790bd7b2694ed685280ac996abc
NinjaAdam <116517776+NinjaAdam@users.noreply.github.com>
2023-03-24 14:49:45 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,24 +1,27 @@
-(function() {
-  // Define block information
-  var blockInfo = {
-    "opcode": "sayBlock",
-    "blockType": "command",
-    "text": "Say [TEXT] in this block",
-    "arguments": {
-      "TEXT": {
-        "type": "string",
-        "defaultValue": "Hello, world!"
-      }
-    }
-  };
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
 
-  // Define block function
-  function sayBlock(args) {
-    var target = this.runtime.getTargetForStage();
-    var text = args.TEXT;
-    target.say(text);
-  }
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
 
-  // Register block with Scratch
-  Scratch.extensions.register("SayBlock", blockInfo, sayBlock);
-})();
+    // Say block
+    ext.sayBlock = function(text) {
+        // Get the current target sprite
+        var target = this.runtime.getSpriteTarget();
+
+        // Make the sprite say the text
+        target.say(text);
+    };
+
+    // Register the extension
+    Scratch.extensions.register('Say Block', {
+        blocks: [
+            [' ', 'say %s in this block', 'sayBlock', 'Hello, world!']
+        ],
+        menus: {},
+    });
+})({});
