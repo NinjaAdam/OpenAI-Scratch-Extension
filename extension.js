5a27986b43eaf88f680bb62687fe099029457644
NinjaAdam <116517776+NinjaAdam@users.noreply.github.com>
2023-03-24 15:31:05 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,22 +1,88 @@
 (function(ext) {
-    // This function is called when the extension is loaded
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+    // Define OpenAI API endpoint and version
+    var openaiEndpoint = "https://api.openai.com";
+    var openaiVersion = "v1";
+
+    // Define Scratch block for generating text
+    ext.generateText = function(apiKey, prompt, variable, callback) {
+        // Construct OpenAI API request URL
+        var requestUrl = openaiEndpoint + "/" + openaiVersion + "/engines/davinci-codex/completions";
+        
+        // Define request headers and body
+        var requestHeaders = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + apiKey
+        };
+        var requestBody = {
+            "prompt": prompt,
+            "max_tokens": 100,
+            "temperature": 0.5,
+            "n": 1,
+            "stop": "\n",
+            "echo": true
+        };
+        
+        // Send request to OpenAI API
+        $.ajax({
+            url: requestUrl,
+            type: "POST",
+            headers: requestHeaders,
+            data: JSON.stringify(requestBody),
+            success: function(response) {
+                // Get generated text from API response
+                var generatedText = response.choices[0].text.trim();
+                
+                // Set Scratch variable to generated text
+                ext.setVariable(variable, generatedText);
+                
+                // Execute Scratch callback function
+                callback();
+            },
+            error: function(xhr, status, error) {
+                // Log error message to console
+                console.log("Error: " + error);
+                
+                // Execute Scratch callback function
+                callback();
+            }
+        });
+    };
+    
+    // Define Scratch block for setting a variable
+    ext.setVariable = function(variable, value) {
+        // Set Scratch variable to value
+        var target = "_stage_";
+        var varId = "☁ " + variable;
+        var data = {
+            "name": variable,
+            "value": value
+        };
+        ScratchExtensions.runtime.sendMessage(target, "setVariable:" + varId + ":" + JSON.stringify(data));
     };
 
-    // This function is called when the "Say" block is executed
-    ext.say_text = function(text) {
-        // Use the Scratch audio engine to say the text
-        scratchAudio.speak(text);
-    };
-
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            [' ', 'Say %s', 'say_text', 'Hello, world!']
-        ]
-    };
-
-    // Register the extension
-    ScratchExtensions.register('Text to Speech', descriptor, ext);
+    // Register Scratch extension
+    ScratchExtensions.register("OpenAI Extension", {
+        // Define Scratch block for generating text
+        generateText: {
+            // Define block information
+            blocks: [
+                ["R", "generate text with API key %s prompt %s set variable %s", "generateText"]
+            ],
+            // Define block arguments
+            arguments: {
+                "API key": {
+                    type: "string",
+                    defaultValue: "your_api_key_here"
+                },
+                "prompt": {
+                    type: "string",
+                    defaultValue: "Enter your prompt here"
+                },
+                "variable": {
+                    type: "string",
+                    defaultValue: "variable_name"
+                }
+            }
+        }
+    });
 })({});
