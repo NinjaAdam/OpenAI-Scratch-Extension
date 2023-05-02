7008c1aa7c74a331e50b8e8e7a487bfda5014e86
NinjaAdam <116517776+NinjaAdam@users.noreply.github.com>
2023-05-01 19:55:56 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,88 +1,51 @@
-(function(ext) {
-    // Define OpenAI API endpoint and version
-    var openaiEndpoint = "https://api.openai.com";
-    var openaiVersion = "v1";
+class AIBlock {
+    constructor() {
+        this.apiKey = "";
+    }
 
-    // Define Scratch block for generating text
-    ext.generateText = function(apiKey, prompt, variable, callback) {
-        // Construct OpenAI API request URL
-        var requestUrl = openaiEndpoint + "/" + openaiVersion + "/engines/davinci-codex/completions";
-        
-        // Define request headers and body
-        var requestHeaders = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + apiKey
-        };
-        var requestBody = {
-            "prompt": prompt,
-            "max_tokens": 100,
-            "temperature": 0.5,
-            "n": 1,
-            "stop": "\n",
-            "echo": true
-        };
-        
-        // Send request to OpenAI API
-        $.ajax({
-            url: requestUrl,
-            type: "POST",
-            headers: requestHeaders,
-            data: JSON.stringify(requestBody),
-            success: function(response) {
-                // Get generated text from API response
-                var generatedText = response.choices[0].text.trim();
-                
-                // Set Scratch variable to generated text
-                ext.setVariable(variable, generatedText);
-                
-                // Execute Scratch callback function
-                callback();
-            },
-            error: function(xhr, status, error) {
-                // Log error message to console
-                console.log("Error: " + error);
-                
-                // Execute Scratch callback function
-                callback();
-            }
-        });
-    };
-    
-    // Define Scratch block for setting a variable
-    ext.setVariable = function(variable, value) {
-        // Set Scratch variable to value
-        var target = "_stage_";
-        var varId = "☁ " + variable;
-        var data = {
-            "name": variable,
-            "value": value
-        };
-        ScratchExtensions.runtime.sendMessage(target, "setVariable:" + varId + ":" + JSON.stringify(data));
-    };
-
-    // Register Scratch extension
-    ScratchExtensions.register("OpenAI Extension", {
-        // Define Scratch block for generating text
-        generateText: {
-            // Define block information
-            blocks: [
-                ["R", "generate text with API key %s prompt %s set variable %s", "generateText"]
-            ],
-            // Define block arguments
-            arguments: {
-                "API key": {
-                    type: "string",
-                    defaultValue: "your_api_key_here"
+    getInfo() {
+        return {
+            "id": "AI",
+            "name": "AI",
+            "blocks": [
+                {
+                    "opcode": "completePrompt",
+                    "blockType": "reporter",
+                    "text": "complete prompt [string]",
+                    "arguments": {
+                        "string": {
+                            "type": "string",
+                            "defaultValue": "Explain quantum computing in simple terms"
+                        }
+                    }
                 },
-                "prompt": {
-                    type: "string",
-                    defaultValue: "Enter your prompt here"
-                },
-                "variable": {
-                    type: "string",
-                    defaultValue: "variable_name"
+                {
+                    "opcode": "setApiKey",
+                    "blockType": "command",
+                    "text": "set API key to [apiKey]",
+                    "arguments": {
+                        "apiKey": {
+                            "type": "string",
+                            "defaultValue": ""
+                        }
+                    }
                 }
-            }
+            ],
+            "menus": {}
+        };
+    }
+
+    setApiKey({ apiKey }) {
+        this.apiKey = apiKey;
+    }
+
+    async completePrompt({ string }) {
+        if (this.apiKey === "") {
+            throw new Error("API key not set.");
         }
-    });
-})({});
+
+        // Rest of the code for the "completePrompt" block
+    }
+}
+
+Scratch.extensions.register(new AIBlock());
